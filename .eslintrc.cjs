e536acf9b3298fc15074724e3afa5e4846337a85
LY <15677355462@163.com>
2023-11-06 18:43:10 +0800

    为项目添加代码规范，eslint+prettier+stylelint+husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
@@ -11,5 +11,15 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  overrides: [
+    {
+      // 匹配views和二级目录中的index.vue
+      files: ['src/views/index.vue', 'src/views/**/index.vue', 'src/components/**/index.vue'],
+      rules: {
+        // 给上面匹配的文件指定规则
+        'vue/multi-word-component-names': 'off'
+      }
+    }
+  ]
 }
