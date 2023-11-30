6dc313f1fc0371c3ba960b8791cf7e182176e4c5
LY <15677355462@163.com>
2023-11-30 17:23:35 +0800

    配置url

--- src/router/index.ts
+++ src/router/index.ts
@@ -34,7 +34,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: routes
 })
 
